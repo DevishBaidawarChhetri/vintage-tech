e190ffd6c2f48f9cc3df2b4ce56c178bdf795c23
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2021-01-20 12:19:32 +0545

    Configured Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React from 'react';
+
+// React Router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
+import Error from './pages/Error';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Login from './pages/Login';
 
 const App = () => {
   return (
-    <div>
-      <h1>hello from app component</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/product/'>
+          <Products />
+        </Route>
+        <Route path='/products/:id' children={<ProductDetails/>}>
+        </Route>
+        <Route path='/cart'>
+          <Cart />
+        </Route>
+        <Route path='/checkout'>
+          <Checkout />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
